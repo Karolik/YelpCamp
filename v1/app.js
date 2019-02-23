77bf453f596a9c8c216e7db5e8f957db3385b88d
Karolina Gauvain <karolina.gauvain@gmail.com>
2019-02-23 18:17:41 +0100

    Create campgrouds, initial routes

--- v1/app.js
+++ v1/app.js
@@ -1,10 +1,37 @@
 var express = require("express");
 var app = express();
+var bodyParser = require("body-parser");
 
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
+var campgrounds = [
+    {name: "Salmon creek", image: "https://farm3.staticflickr.com/2116/2164766085_0229ac3f08.jpg"},
+    {name: "Granite Hill", image: "https://farm2.staticflickr.com/1363/1342367857_2fd12531e7.jpg"},
+    {name: "Mountain Goat", image: "https://farm1.staticflickr.com/130/321487195_ff34bde2f5.jpg"}
+];
+
 app.get("/", function(req, res){
-    res.send("Hi There");
+    res.render("landing");
+});
+
+app.get("/campgrounds", function(req, res){
+    res.render("campgrounds", {campgrounds: campgrounds});
+});
+
+app.post("/campgrounds", function(req, res){
+    //get data from form and add to campgrounds array
+    var name = req.body.name;
+    var image = req.body.image;
+    var newCampground = {name: name, image: image};
+    campgrounds.push(newCampground);
+    
+    //redirect back to campgrounds page
+    res.redirect("/campgrounds");
+});
+
+app.get("/campgrounds/new", function(req, res){
+    res.render("new.ejs");
 });
 
 app.listen(process.env.PORT, process.env.IP, function(){
