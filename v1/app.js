73a30625ced1ae65c9d16a769f1cd668c9c47095
Karolina Gauvain <karolina.gauvain@gmail.com>
2019-03-14 22:18:43 +0100

    Add mongoose and show route

--- v1/app.js
+++ v1/app.js
@@ -1,37 +1,85 @@
-var express = require("express");
-var app = express();
-var bodyParser = require("body-parser");
+var express = require("express"),
+    app = express(),
+    bodyParser = require("body-parser"),
+    mongoose = require("mongoose");
 
+mongoose.connect("mongodb://localhost/yelp_camp", { useNewUrlParser: true });
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
-var campgrounds = [
-    {name: "Salmon creek", image: "https://farm3.staticflickr.com/2116/2164766085_0229ac3f08.jpg"},
-    {name: "Granite Hill", image: "https://farm2.staticflickr.com/1363/1342367857_2fd12531e7.jpg"},
-    {name: "Mountain Goat", image: "https://farm1.staticflickr.com/130/321487195_ff34bde2f5.jpg"}
-];
+//SCHEMA Setup
+var campgroundSchema = new mongoose.Schema({
+    name: String,
+    image: String,
+    description: String
+});
+
+var Campground = mongoose.model("Campground", campgroundSchema);
+
+/*Campground.create(
+    {
+        name: "Granite Hill", 
+        image: "https://farm2.staticflickr.com/1363/1342367857_2fd12531e7.jpg",
+        description: "This is a Granite Hill campground!"
+    }, function(err, campground){
+        if(err){
+            console.log(err);
+        } else{
+            console.log("Newly created campground:");
+            console.log(campground);
+        }
+    });*/
 
 app.get("/", function(req, res){
     res.render("landing");
 });
 
+//INDEX - show all capgrounds
 app.get("/campgrounds", function(req, res){
-    res.render("campgrounds", {campgrounds: campgrounds});
+    //Get all campgrounds from DB
+    Campground.find({}, function(err, allCampgrounds){
+        if(err){
+            console.log(err);
+        }else{
+            res.render("index", {campgrounds: allCampgrounds});
+        }
+    });
 });
 
 app.post("/campgrounds", function(req, res){
     //get data from form and add to campgrounds array
     var name = req.body.name;
     var image = req.body.image;
-    var newCampground = {name: name, image: image};
-    campgrounds.push(newCampground);
-    
-    //redirect back to campgrounds page
-    res.redirect("/campgrounds");
+    var desc = req.body.description;
+    var newCampground = {name: name, image: image, description: desc};
+    //Create a new campground and save it to DB
+    Campground.create(newCampground, function(err, newlyCreated){
+        if(err){
+            console.log(err);
+        }else{
+            //redirect back to campgrounds page
+            res.redirect("/campgrounds");
+        }
+    });
 });
 
+//NEW
 app.get("/campgrounds/new", function(req, res){
     res.render("new.ejs");
+});
+
+//SHOW - shows more info about one campground
+app.get("/campgrounds/:id", function(req, res){
+    //find the campground with provided id
+    Campground.findById(req.params.id, function(err, foundCampground){
+        if(err){
+            console.log(err);
+        } else {
+            //render show template with that campground
+            res.render("show", {campground: foundCampground});
+        }
+    });
+    req.params.id;
 });
 
 app.listen(process.env.PORT, process.env.IP, function(){
