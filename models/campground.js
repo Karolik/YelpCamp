4ff8118ef1db4760c5ca864ceef20ef7bf50aa70
Karolina Gauvain <karolina.gauvain@gmail.com>
2019-05-12 16:23:40 +0200

    Associate users and campgrounds

--- models/campground.js
+++ models/campground.js
@@ -1,10 +1,16 @@
 var mongoose = require("mongoose");
 
-//SCHEMA Setup
 var campgroundSchema = new mongoose.Schema({
    name: String,
    image: String,
    description: String,
+   author: {
+      id: {
+         type: mongoose.Schema.Types.ObjectId,
+         ref: "User"
+      },
+      username: String
+   },
    comments: [
       {
          type: mongoose.Schema.Types.ObjectId,
@@ -12,5 +18,5 @@ var campgroundSchema = new mongoose.Schema({
       }
    ]
 });
- 
+
 module.exports = mongoose.model("Campground", campgroundSchema);
