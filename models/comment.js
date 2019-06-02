132815d477b3d7ceaa66cca121c6eb9fffb3c33d
Karolina Gauvain <karolina.gauvain@gmail.com>
2019-06-02 19:31:00 +0200

    Change var to const

--- models/comment.js
+++ models/comment.js
@@ -1,14 +1,14 @@
-var mongoose = require("mongoose");
+const mongoose = require("mongoose");
 
-var commentSchema = mongoose.Schema({
-    text: String,
-    author: {
+const commentSchema = mongoose.Schema({
+      text: String,
+      author: {
         id: {
             type: mongoose.Schema.Types.ObjectId,
             ref: "User"
         },
         username: String
-    }
+      }
 });
 
 module.exports = mongoose.model("Comment", commentSchema);
